48699361e44475e8980b2faa17c1f553fbf95198
LyudmilaStear <138987986+LyudmilaStear@users.noreply.github.com>
2023-07-25 17:51:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SeminarsCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
