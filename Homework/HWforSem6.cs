c749f41e7d97df7bfcd04bb651da3291ac0b964c
LyudmilaStear <138987986+LyudmilaStear@users.noreply.github.com>
2023-08-25 17:22:46 +0300

    Дз к сем 6 с доп заданием

--- Homework/HWforSem6.cs
+++ Homework/HWforSem6.cs
@@ -8,13 +8,14 @@ namespace SeminarsCsharp.Homework
 {
     public static class HWforSem6
     {
-        public static void Launch() 
+        public static void Launch()
         {
 
             //Task1.Пользователь вводит с клавиатуры M чисел.
             //Посчитайте, сколько чисел больше 0 ввёл пользователь. с/без массива, но с методом.
             //0, 7, 8, -2, -2-> 2
             //1, -7, 567, 89, 223-> 3
+
             /*
              void WriteArray(int[] array)
             {
@@ -60,6 +61,33 @@ namespace SeminarsCsharp.Homework
             Console.WriteLine($"количество чисел больше нуля - {CountPosNum(userArr)}");
             */
 
+            /*
+            int Mes(string message)
+            {
+                Console.WriteLine(message);
+                string value = Console.ReadLine();
+                int result = Convert.ToInt32(value);
+                return result;
+            }
+
+            int UserNumbrs(int numAmount)
+            {
+                int count = 0;
+                for (int i = 1; i <= numAmount; i++)
+                {
+                    Console.WriteLine($"Введите {0 + i}-ое число для проверки");
+                    int numb = Convert.ToInt32(Console.ReadLine());
+                    if (numb > 0) count++;
+                }
+                return count;
+            }
+
+            Console.WriteLine("Введите, кол-во чисел, которое планируется проверять");
+            int numToCheck = Convert.ToInt32(Console.ReadLine());
+            int posNumbrs = UserNumbrs(numToCheck);
+            Console.WriteLine($"Веедено {posNumbrs} положительных чисел");
+            */
+
 
             //Task2.Напишите программу, которая найдёт точку пересечения двух прямых,
             //заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
@@ -74,6 +102,7 @@ namespace SeminarsCsharp.Homework
             // уравнение полученное значение «x», найдём координату
             // «y» точки пересечения двух прямых.
 
+            /*
             float WriteWait(string outLine)
             {
                 Console.Write(outLine);
@@ -104,16 +133,54 @@ namespace SeminarsCsharp.Homework
             float inB2 = WriteWait("Введите смещение b второй прямой: ");
 
             СalculationIntersection(inK1, inK2, inB1, inB2);
-
+            */
 
             //Task3.Extra. Написать программу, преобразующую число
             //из десятеричной системы счисления в двоичную.
+            /*
+            int num1 = 123 / 2; //61
+            double num2 = 123 % 2; //1
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            int num1_2 = 123 / 10; //12
+            double num2_2 = 123 % 10; //3
+            Console.WriteLine(num1_2);
+            Console.WriteLine(num2_2);
+            */
 
+            string Transform10Into2(int num)
+            {
+                string numIn2 = String.Empty;
+                while (num > 0)
+                {
+                    int dig = num % 2;
+                    num = num / 2;
+                    //numIn2 = Convert.ToString(dig) + numIn2; // и на этом все
+                    numIn2 = numIn2 + Convert.ToString(dig);
+                }
+                
+                Console.WriteLine(numIn2);
+                
+                string revNumIn2 = String.Empty;
 
+                int oldNum = Convert.ToInt32(numIn2);
+                int i = 0;
+             
+                while (i < numIn2.Length)
+                {
+                    int rightDigit = oldNum % 10;
+                    oldNum = oldNum / 10;
+                    i++;
 
+                    revNumIn2 = revNumIn2 + Convert.ToString(rightDigit);
+                }
 
+                return revNumIn2;
+            }
 
-
+            Console.WriteLine("Введите число для перевода в двоичную систему ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Transform10Into2(userNum));
         }
 
     }
