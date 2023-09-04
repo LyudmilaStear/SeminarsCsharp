e0db4d3e548c235d0fcfe05d43e402e9abed2e90
LyudmilaStear <138987986+LyudmilaStear@users.noreply.github.com>
2023-09-04 18:50:31 +0300

    Homework9

--- Homework/HWforSem9.cs
+++ Homework/HWforSem9.cs
@@ -73,11 +73,33 @@ namespace SeminarsCsharp.Homework
             //m = 2, n = 3->A(m, n) = 9
             //m = 3, n = 2->A(m, n) = 29
 
+           //функция ack(n, m)
+           //         если n = 0
+           //  вернуть m +1
+           //иначе, если m = 0
+           //  вернуть ack(n -1, 1)
+           //еще
+           //  вернуть ack(n - 1, ack(n, m - 1))
+
             int AccermanFunc(int a, int b)
             {
-
+                if (a == 0)
+                    return b + 1;
+                else 
+                {
+                    if (b == 0)
+                        return AccermanFunc(a - 1, 1);
+                    else
+                        return AccermanFunc(a - 1, AccermanFunc(a, b - 1));
+                }
             }
+            Console.Write("Input the first number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the second number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
 
+            int res = AccermanFunc(number1, number2);
+            Console.WriteLine($"Accerman func ({number1}, {number2}) is {res}");
         }
     }
 }
